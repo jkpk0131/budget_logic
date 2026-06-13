3892957750315db65896f9a498af34a15d78e451
jkpk0131 <jkpk0131@gmail.com>
2026-06-13 01:41:31 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = "budget-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "https://esm.sh/preact@10.19.6",
+  "https://esm.sh/preact@10.19.6/hooks",
+  "https://esm.sh/htm@3.1.1",
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {}));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+      if (res.ok) {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    })).catch(() => caches.match("./index.html"))
+  );
+});
